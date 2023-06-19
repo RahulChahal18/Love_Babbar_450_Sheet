0a0da57a6911f55df02f48269e2bd4de0a4cf3ab
RahulChahal18 <124885254+RahulChahal18@users.noreply.github.com>
2023-06-19 15:46:16 +0530

    Create Check_if_strings_are_rotations_of_each_other_2.cpp

---
+++ Strings/Check_if_strings_are_rotations_of_each_other_2.cpp
@@ -0,0 +1,17 @@
+
+class Solution
+{
+    public:
+    //Function to check if two strings are rotations of each other or not.
+    bool areRotations(string s1,string s2)
+    {
+        // Your code here
+        if(s1.length()!=s2.length())
+            return false;
+            
+        string temp = s1+s1;
+        // Check if the second string is a substring of the concatenated string
+        bool b= temp.find(s2)!= string::npos;
+        return b;
+    }
+};
